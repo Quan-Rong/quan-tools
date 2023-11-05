f763f160dbb444763449c3435d8049d0760a95ba
Quan-Rong <91610165+Quan-Rong@users.noreply.github.com>
2023-11-05 12:52:30 +0000

    statistic analysis start

--- Hello.py
+++ Hello.py
@@ -20,11 +20,11 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Hello Mr. Rong",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to Streamlit! How are you 14 👋")
 
     st.sidebar.success("Select a demo above.")
 
