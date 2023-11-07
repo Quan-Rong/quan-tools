6ed875a3d9d8ba8629ac00e5586726a8fb550e2e
Quan-Rong <91610165+Quan-Rong@users.noreply.github.com>
2023-11-08 00:13:55 +0100

    Add files via upload

---
+++ pages/6_🦜LongChain.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from PIL import Image
+from langchain.llms import OpenAI
+
+st.set_page_config(
+        layout="wide"
+    )
+
+#st.set_page_config(page_title="🦜🔗 Quickstart App")
+st.title('🦜🔗 Gestamp Chassis GPT Self-Training Modell')
+
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+longchain_col1, longchain_col2, longchain_col3 = st.columns([1, 1, 1]) 
+with longchain_col2:
+    longchain_image=Image.open('image/Longchain.JPG')
+    st.image(longchain_image, caption='Version: Beta V0.1')
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  ##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
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the most common forms of rear suspension in cars?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
+    
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
