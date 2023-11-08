24dfab7d0b59564acf64f261c188a438a90f50ed
Quan-Rong <91610165+Quan-Rong@users.noreply.github.com>
2023-11-08 01:03:15 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,41 +13,64 @@
 # limitations under the License.
 
 import streamlit as st
-from streamlit.logger import get_logger
+from PIL import Image
 
 
-LOGGER = get_logger(__name__)
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋", layout="wide"
+)
 
+st.write('''# The Tools''')
 
+main_image=Image.open('logo_main_11.JPG')
+st.image(main_image, caption='Version: Beta V1.0')
 
-def run():
-    st.set_page_config(
-        page_title="Hello Mr. Rong",
-        page_icon="👋",layout="wide"
-    )
+st.sidebar.success("Select a locacase above.")
 
-    st.write("# Welcome to Streamlit! How are you 14 👋")
+st.markdown('''---''')
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+st.markdown(
     """
-    )
+    * Complete APP deployment, local/cloud. Docker installation runs on Linux.
+    * Optimize K&C result analysis.
+    * Integrate Static Loads Generation feature.
+    * Integrate KnC database.
+    * Integrate suspension computer.
+"""
+)
 
+st.markdown('''---''')
 
-if __name__ == "__main__":
-    run()
+def main():
+    cs_body()
+    
+def cs_body():
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        main_image_01=Image.open('logo_main_12.JPG')    
+        st.image(main_image_01)
+        st.write("✈️K&C Simulation Results PostProcess")
+    
+        main_image_02=Image.open('logo_main_13.JPG')    
+        st.image(main_image_02)
+        st.write("🤸🏻Gestamp Static Loads")
+
+# 在第二个列中显示上下两张图片，并在每张图片下面写一行字
+    with col2:
+        main_image_03=Image.open('logo_main_14.JPG')    
+        st.image(main_image_03)
+        st.write("👨🏻‍💻K&C DataBase Analysis")
+    
+        main_image_04=Image.open('logo_main_15.JPG')    
+        st.image(main_image_04)
+        st.write("🕵🏻Suspension Parameter Calculation")
+
+    return None
+
+# Run main()
+
+if __name__ == '__main__':
+    main()
